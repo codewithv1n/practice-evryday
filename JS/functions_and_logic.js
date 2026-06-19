3cf4b8d04c7736dcdfe18e31470709ad728d334a
codewithv1n <vinzelmarano@gmail.com>
2026-06-19 21:00:50 +0800

    Initial Commit -- if/else -- only done

---
+++ JS/functions_and_logic.js
@@ -0,0 +1,16 @@
+// Gumawa ng isang function na tumatanggap ng dalawang numero (a, b) at nag-re-return ng sum nila.
+
+// Gumawa ng if-else statement: Kung ang numero ay positive, i-print ang "Positive", kung negative, i-print ang "Negative".
+
+const number = 5;
+
+if (number > 0) {
+  console.log("Positive");
+
+} else if (number < 0) {
+
+  console.log("Negative");
+}
+ 
+
+// Gumawa ng For Loop na mag-lo-loop mula 1 hanggang 10 at i-print lang ang mga even numbers.
