f6f103c406d56dc1218af5874df8874d6bad9da0
codewithv1n <vinzelmarano@gmail.com>
2026-06-20 21:06:00 +0800

    Initial Commit -- function return sum of two numbers

--- JS/functions_and_logic.js
+++ JS/functions_and_logic.js
@@ -1,16 +1,24 @@
 // Gumawa ng isang function na tumatanggap ng dalawang numero (a, b) at nag-re-return ng sum nila.
+function number (a, b ){
+  return a + b;
+}
+
+const result = number (5,5);
+console.log(result);
+
 
 // Gumawa ng if-else statement: Kung ang numero ay positive, i-print ang "Positive", kung negative, i-print ang "Negative".
 
-const number = 5;
+// const number = 5;
 
-if (number > 0) {
-  console.log("Positive");
+// if (number > 0) {    // greater than
 
-} else if (number < 0) {
+//  console.log("Positive");
+ 
+// } else if (number < 0) {  // less than 
 
-  console.log("Negative");
-}
+//  console.log("Negative");
+// }
  
 
 // Gumawa ng For Loop na mag-lo-loop mula 1 hanggang 10 at i-print lang ang mga even numbers.
